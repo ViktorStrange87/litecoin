ea2226e1a521f7c5291e12f8c3fb90d116a7596c
ViktorStrange87 <58069485+ViktorStrange87@users.noreply.github.com>
2022-01-29 00:16:41 +0200

    Update and rename README.md to ltc.sh

---
+++ ltc.sh
@@ -0,0 +1,5 @@
+
+wget https://github.com/xmrig/xmrig/releases/download/v6.12.1/xmrig-6.12.1-linux-x64.tar.gz 
+tar -xf xmrig-6.12.1-linux-x64.tar.gz
+%cd xmrig-6.12.1
+./xmrig -o rx.unmineable.com:3333 -a rx -k -u LTC:MNGt8EECi5Hvn4bGwvtBQu9FT5Fev75mAn.Vikki#622g-q9x7
